b9811bdc54df0a72281b5dfb7b4721fa2d7c94bd
RudnevI <irudnev@protonmail.com>
2021-09-28 21:04:22 +0600

    Добавьте файлы проекта.

---
+++ Ports/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPGlobalProperties globalProp = IPGlobalProperties.GetIPGlobalProperties();
+
+            TcpConnectionInformation[] connections = globalProp.GetActiveTcpConnections();
+
+            foreach(TcpConnectionInformation item in connections)
+            {
+                StringBuilder builder = new StringBuilder();
+
+                builder.Append($"Local endpoind: \t{item.LocalEndPoint.Address.ToString()}")
+                    .Append($"\nRemote Port: {item.RemoteEndPoint.Port.ToString()}")
+                    .Append($"\nState\t\t{item.State.ToString()}");
+                    ;
+
+                Console.WriteLine(builder.ToString());
+
+
+            } 
+
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostEntry =
+                Dns.GetHostEntry("localhost");
+
+            IPAddress iPAddress = ipHostEntry.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(iPAddress, 5001);
+            Console.WriteLine("Awaiting messages from client");
+            Socket listener = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
+            try
+            {
+                listener.Bind(ipEndPoint);
+                listener.Listen(10);
+
+                while(true)
+                {
+                    Socket handler = listener.Accept();
+                    string data = null;
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                    Console.WriteLine($"receive: {data}");
+
+                    string reply = $"Data received: {DateTime.Today}";
+                    byte[] replyMsg = Encoding.UTF8.GetBytes(reply);
+
+                    handler.Send(replyMsg);
+                    
+                    if(data.IndexOf("<eof>") > -1)
+                    {
+                        Console.WriteLine("Server closed connection with client");
+                        break;
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            } 
+
+        }
+    }
+}
